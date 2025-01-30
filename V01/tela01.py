3be0e88e1e10b38614fdee7a7a486c827b916816
Erika Bernardo Lima <20242014040026@ifrn.local>
2025-01-30 16:26:16 -0300

    Atualização da tela

--- V01/tela01.py
+++ V01/tela01.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Inicializa o Pygame
 pygame.init()
@@ -9,17 +10,21 @@ tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Alimente o gatinho")
 
 # Imagem de fundo
-fundo = pygame.image.load("V01/fundo.webp")
+fundo = pygame.image.load("./fundo.webp")
 
 # Imagem gatinho
-gatinhoBocaFechada = pygame.image.load("V01/bocaFechada.png")
+gatinhoBocaFechada = pygame.image.load("./bocaFechada.png")
 gatinhoBocaFechada = pygame.transform.scale(gatinhoBocaFechada, (100, 100))
-petisco = pygame.image.load("V01/petisco.png")
+petisco = pygame.image.load("./petisco.png")
 petisco = pygame.transform.scale(petisco, (80, 80))
+
+naverect = petisco.get_rect()
+naverect.y = 240
+naverect.x = 320
 
 
 # Posição inicial da imagem petisco
-x = 0 # Termina no final da tela
+x = random.randint(0, largura - petisco.get_width())  # Posição aleatória dentro da tela
 y = 0 # Inicia no início da tela
 
 # Velocidada da queda 
@@ -28,9 +33,27 @@ velocidade = 5
 # Loop principal
 rodando = True
 while rodando:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:  # Fecha a janela
-            rodando = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        naverect.x -= 5
+        if naverect.left < 0:
+            naverect.left = 0
+    if keys[pygame.K_RIGHT]:
+        naverect.x += 5
+        if naverect.right > 640:
+            naverect.right = 640
+    if keys[pygame.K_DOWN]:
+        naverect.y += 5
+        if naverect.bottom > 480:
+            naverect.bottom = 480
+    if keys[pygame.K_UP]:
+        naverect.y -= 5
+        if naverect.top < 0:
+            naverect.top = 0
 
     # Define o fundo branco
     # tela.fill((255, 255, 255)) 
@@ -41,6 +64,8 @@ while rodando:
     # Se a imagem atingir o fundo da tela, ela volta para o topo
     if y > altura:  
         y -80 # Reinicia acima da tela
+        x = random.randint(0, largura - petisco.get_width())  # Gera uma nova posição aleatória para x
+
 
 
     tela.blit(fundo,(0,0))
